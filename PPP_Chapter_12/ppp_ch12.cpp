26b8af1aebfb6082bca72060cb15ed7b71f8d6f2
stelatoris <78478872+stelatoris@users.noreply.github.com>
2021-10-07 18:05:39 +0200

    Ex 07 done.

--- PPP_Chapter_12/ppp_ch12.cpp
+++ PPP_Chapter_12/ppp_ch12.cpp
@@ -86,159 +86,56 @@ void Arrow::draw_lines() const
     }
 }
 
-
-
-
-
-
-
 int main()
 try
 {
     // Window
     Point tl{ 100, 100 };
-    Simple_window win{ tl, 1000, 600, "Chapter 13 Ex 1" };
-
+    Simple_window win{ tl, 2000, 1200, "Chapter 13 Ex 1" };
     
+    // 7. Make an RGB color chart (e.g., search the web for “RGB color chart”).
+    Vector_ref<Graph_lib::Rectangle>vr;
+        
+    int s_w{ 20 };
+    int s_h{ 20 };
+
+    int R{ 270 };
+    int G{ 0 };
+    int B{ 0 };
     
-    
-    int x_size=win.x_max();
-    int y_size=win.y_max();
+    for (int r = 0; r < 2; ++r) {
+        int row = r * 200;        
 
-    Lines grid_50x50;
+        for (int b = 0; b < 5; ++b) {
+            int column = b * 200;
+            if (b == 4 && r==1 ) B = 255;   // last set
 
-    for(int x=50; x<x_size; x+=50)
-      grid_50x50.add(Point{x,0},Point{x,y_size});
+            for (int i = 0; i < 10; ++i) {  // i is for Y axis
+                if (i == 0) R = 255;
 
-    for(int y=50; y<y_size; y+=50)
-      grid_50x50.add(Point{0,y},Point{x_size,y});
+                for (int j = 0; j < 10; ++j) {  // j is for X axis
+                    
+                    if (j == 9) G = 255;             
 
-    grid_50x50.set_color(Color::red);
-    win.attach(grid_50x50);
+                    vr.push_back(new Graph_lib::Rectangle{ Point{column + j * s_w,row+i * s_h},s_w,s_h });
 
-      Lines grid_100x100;
+                    vr[vr.size() - 1].set_fill_color(fl_rgb_color(R, G, B));
 
-    for(int x=100; x<x_size; x+=100)
-      grid_100x100.add(Point{x,0},Point{x,y_size});
-
-    for(int y=100; y<y_size; y+=100)
-      grid_100x100.add(Point{0,y},Point{x_size,y});
-
-    grid_100x100.set_color(Color::green);
-    win.attach(grid_100x100);
-    
-
-    // Arc tests
-    //Graph_lib::Arc ar1{ Point{400,300},200,100,1,180 };
-    //ar1.set_color(Color::red);
-    //win.attach(ar1);
-
-    //win.wait_for_button();
-
-    /*
-    Graph_lib::Rounded_box rb1{ tl, 576,367,32 };
-    rb1.set_color(Color::blue);
-
-    win.attach(rb1);
-
-    win.wait_for_button();    
-    */
-
-    //-----------------------------------------------------------
-    // Ex-03
-    /*
-    Graph_lib::Arrow ar1{ Point{300,300}, Point{500,200} };
-    ar1.set_head_angle(20);
-    ar1.set_head_size(50);
-    win.attach(ar1);
-    win.wait_for_button();
-    */
-
-    Circle c1{ Point{100,200},50 };
-    win.attach(c1);
-
-    Point pc_center = center(c1);
-    Mark m_c_center{ pc_center,'x' };
-    win.attach(m_c_center);
-
-    Point pcn = n(c1);
-    Mark m_cn{ pcn,'x' };
-    win.attach(m_cn);
-
-    Point pce = e(c1);
-    Mark m_ce{ pce,'x' };
-    win.attach(m_ce);
-
-    Point pcs = s(c1);
-    Mark m_cs{ pcs,'x' };
-    win.attach(m_cs);
-
-    Point pcw = w(c1);
-    Mark m_cw{ pcw,'x' };
-    win.attach(m_cw);
-
-    Point pcne = ne(c1);
-    Mark m_cne{ pcne,'x' };
-    win.attach(m_cne);
-
-    Point pcse = se(c1);
-    Mark m_cse{ pcse,'x' };
-    win.attach(m_cse);
-
-    Point pcsw = sw(c1);
-    Mark m_csw{ pcsw,'x' };
-    win.attach(m_csw);
-
-    Point pcnw = nw(c1);
-    Mark m_cnw{ pcnw,'x' };
-    win.attach(m_cnw);
+                    win.attach(vr[vr.size() - 1]);
+                    G += 30;
+                }
+                if (i == 0) R = 270;
+                R -= 30;
+                G = 0;
+            }
+            R = 270;
+            G = 0;
+            B += 30;
+        }
+        
+    }
 
     win.wait_for_button();
-
-    Graph_lib::Ellipse e1{ Point{300,400},200,100 };
-    win.attach(e1);
-
-    Point pe_center = center(e1);
-    Mark m_e_center{ pe_center,'x' };
-    win.attach(m_e_center);
-    
-    Point pen = n(e1);
-    Mark m_en{ pen,'x' };
-    win.attach(m_en);
-
-    Point pes = s(e1);
-    Mark m_es{ pes,'x' };
-    win.attach(m_es);
-
-    Point pee = e(e1);
-    Mark m_ee{ pee,'x' };
-    win.attach(m_ee);
-
-    Point pew = w(e1);
-    Mark m_ew{ pew,'x' };
-    win.attach(m_ew);
-    
-    Point pene = ne(e1);
-    Mark m_ene{ pene,'x' };
-    win.attach(m_ene);
-
-    Point pese = se(e1);
-    Mark m_ese{ pese,'x' };
-    win.attach(m_ese);
-
-    Point pesw = sw(e1);
-    Mark m_esw{ pesw,'x' };
-    win.attach(m_esw);
-
-    Point penw = nw(e1);
-    Mark m_enw{ penw,'x' };
-    win.attach(m_enw);
-
-    win.wait_for_button();
-
-    //-----------------------------------------------------------
-
-
 }
 
 catch (std::exception& e) {
